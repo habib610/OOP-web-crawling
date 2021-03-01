6a00022ec9df70be21566026462d8ced8c32a215
habib610 <habib54562@gmail.com>
2021-03-01 21:07:45 +0600

    object-and-class

---
+++ object-class.py
@@ -0,0 +1,16 @@
+import turtle
+
+# turtle.circle(100)
+# turtle.speed(8                                            )
+#
+# turtle.done()
+
+nonte = turtle.Turtle()
+fonte = turtle.Turtle()
+
+nonte.shape('circle')
+fonte.shape("square")
+nonte.left(30)
+nonte.forward(100)
+nonte.speed(5)
+fonte.backward(50)
